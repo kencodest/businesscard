d1c7799628066f70a9995f58d0c8e1f46d0e3f0f
kencodest <muiaken88@gmail.com>
2022-06-16 00:16:22 +0300

    edit Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { IconContext } from "react-icons";
-import { FaTwitterSquare } from "react-icons/fa"
-import { FaFacebookSquare } from "react-icons/fa"
-import { FaInstagramSquare } from "react-icons/fa"
-import { FaGithubSquare } from "react-icons/fa"
+// import { FaTwitterSquare } from "react-icons/fa"
+// import { FaFacebookSquare } from "react-icons/fa"
+// import { FaInstagramSquare } from "react-icons/fa"
+// import { FaGithubSquare } from "react-icons/fa"
 
 function Footer(){
     return(
-        <IconContext.Provider value={{ size: "2.2rem", color: "#918E9B" }}>
+        // <IconContext.Provider value={{ size: "2.2rem", color: "#918E9B" }}>
         <div className="footer">
-            {/* <a href="#" target="_blank"><FaTwitterSquare /></a>
-            <a href="#" target="_blank"><FaFacebookSquare /></a>
-            <a href="#" target="_blank"><FaInstagramSquare /></a>
-            <a href="#" target="_blank"><FaGithubSquare /></a> */}
-            <p>&copy; 2022 All Righs Reserved | <strong><a target="_blank" href="https://github.com/kencodest/businesscard">kencodest</a></strong></p>
+            {/* <a href="#" target="_blank" rel="noreferrer"><FaTwitterSquare /></a>
+            <a href="#" target="_blank" rel="noreferrer"><FaFacebookSquare /></a>
+            <a href="#" target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
+            <a href="#" target="_blank" rel="noreferrer"><FaGithubSquare /></a> */}
+            <p>&copy; 2022 All Righs Reserved | <strong><a target="_blank" href="https://github.com/kencodest/businesscard" rel="noreferrer">kencodest</a></strong></p>
         </div>
-        </IconContext.Provider>
+        // </IconContext.Provider>
         
     )
 }
